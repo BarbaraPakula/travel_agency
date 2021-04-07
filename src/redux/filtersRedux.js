43a42898b85779b2f1628de96a361861a1269386
Barbara Pakula <barbara.pakula@hotmail.com>
2021-04-07 19:18:01 +0200

    prettiera for filtersRedux

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -6,19 +6,26 @@ export const getAllFilters = ({ filters }) => filters;
 
 // action name creator
 const reducerName = 'filters';
-const createActionName = name => `app/${reducerName}/${name}`;
+const createActionName = (name) => `app/${reducerName}/${name}`;
 
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
 export const CHANGE_TAG = createActionName('CHANGE_TAG');
-// TODO - add other action types
 
 // action creators
-export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
-// TODO - add other action creators
-export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
-export const changeTags = payload => ({ payload, type: CHANGE_TAG });
+export const changeSearchPhrase = (payload) => ({
+  payload,
+  type: CHANGE_PHRASE,
+});
+export const changeDuration = (payload) => ({
+  payload,
+  type: CHANGE_DURATION,
+});
+export const changeTags = (payload) => ({
+  payload,
+  type: CHANGE_TAG,
+});
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -33,16 +40,25 @@ export default function reducer(statePart = [], action = {}) {
       return {
         ...statePart,
         duration: {
-          from: action.payload.type === 'from' && +action.payload.value <= +statePart.duration.to ? action.payload.value : statePart.duration.from,
-          to: action.payload.type === 'to' && +action.payload.value >= +statePart.duration.from ? action.payload.value : statePart.duration.to,
+          from:
+            action.payload.type === 'from' &&
+              +action.payload.value <= +statePart.duration.to
+              ? action.payload.value
+              : statePart.duration.from,
+          to:
+            action.payload.type === 'to' &&
+              +action.payload.value >= +statePart.duration.from
+              ? action.payload.value
+              : statePart.duration.to,
         },
       };
 
     case CHANGE_TAG:
       return {
         ...statePart,
-        tags: action.payload.checked ? [...statePart.tags, action.payload.tag] :
-          statePart.tags.filter(tag => tag != action.payload.tag),
+        tags: action.payload.checked
+          ? [...statePart.tags, action.payload.tag]
+          : statePart.tags.filter((tag) => tag != action.payload.tag),
       };
 
     default:
